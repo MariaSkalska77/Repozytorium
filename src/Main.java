aec34d6e26ff336a1e0e7869bdf1d1fbdaf5f412
U6068594 <mariaskalska7@gmail.com>
2019-02-23 12:19:58 +0100

    wiadomosc

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        /*System.out.println("Maria Skalska");
+        System.out.println("Witaj");
+        System.out.println("Maria");*/
+
+        int marysia = 15;
+                System.out.println(marysia);
+
+
+
+
+
+    }
+}
